be4e066c187f0e84e56093dbd20ff6d253377963
msrocka <michael.srocka@gmail.com>
2017-10-10 09:40:58 +0200

    make.bat: create windows and linux version

--- make.bat
+++ make.bat
@@ -1,11 +1,23 @@
 @echo off
 
+if not exist ".\build" mkdir ".\build"
+
 set GOARCH=amd64
 set GOOS=windows
 
-echo Compile backend ...
-if not exist ".\build" mkdir ".\build"
+echo Compile version for %GOOS%_%GOARCH%
+
 cd src
 go build -o ..\build\app.exe
+
+
+set CGO_ENABLED=0
+set GOARCH=amd64
+set GOOS=linux
+
+echo Compile version for %GOOS%_%GOARCH%
+
+go build -o ..\build\app
+
 cd ..
 echo ... all done
