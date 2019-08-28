e7f02927fe056a507a3a0a27591d29dad19d6981
msrocka <michael.srocka@gmail.com>
2019-08-28 13:05:30 +0200

    updated API doc build

--- apidoc/gulpfile.js
+++ apidoc/gulpfile.js
@@ -1,16 +1,23 @@
 const clean = require('gulp-clean');
 const gulp = require('gulp');
 
-gulp.task('default', ['clean'], () => {
+gulp.task('build', (cb) => {
     require('bootprint')
         .load(require('bootprint-openapi'))
         .merge({ /* Any other configuration */ })
         .build('apidoc.yaml', 'build')
-        .generate();
+        .generate()
+        .done(console.log);
+    if (cb) {
+        cb();
+    }
 });
 
 gulp.task('clean', () => {
-    return gulp.src(['build'], {
-        read: false
+    return gulp.src('build', {
+        read: false,
+        allowEmpty: true,
     }).pipe(clean({ force: true }));
 });
+
+gulp.task('default', gulp.series('clean', 'build'));
