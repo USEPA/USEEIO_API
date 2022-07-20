91d2ce0133d6f445cdb49647347cae210ac2aca6
msrocka <michael.srocka@gmail.com>
2022-07-20 11:24:41 +0200

    also test the new USEEIO matrices
    this even works for the vectors q and x

--- tests/test_matrices.py
+++ tests/test_matrices.py
@@ -18,7 +18,11 @@ class MatrixTest(unittest.TestCase):
         client = Client.get()
         for model in client.get_models():
             model_id = model['id']
-            for matrix in ['A', 'B', 'C', 'D', 'L', 'N']:
+            matrices = [
+                'A', 'A_d', 'B', 'C', 'D', 'L', 'L_d', 'M', 'M_d',
+                'N', 'N_d', 'Phi', 'q', 'Rho', 'U', 'U_d', 'V', 'x'
+            ]
+            for matrix in matrices:
                 log.info('check matrix %s in model %s', matrix, model_id)
                 M = client.get_matrix(model_id, matrix)
                 rows, cols = M.shape
